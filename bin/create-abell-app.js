a0c01a077395915778677f26a5b54ada264a8a78
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-26 22:49:46 +0530

    refactor, templates upgrade to 0.5

--- bin/create-abell-app.js
+++ bin/create-abell-app.js
@@ -1,78 +1,130 @@
 #!/usr/bin/env node
-
-const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
+
 
 const prompts = require('prompts');
+const { program } = require('commander');
 
-const {
-  copyFolderSync,
-  rmdirRecursiveSync, 
-  colors
-} = require('../lib/helpers.js');
+const { copyFolderSync, colors, rmdirRecursiveSync } = require('../lib/utils.js');
 
-const { getInstaller, getTemplate } = require('../lib/create-utils.js');
+program.version(require('../package.json').version, '-v|--version');
 
-async function createAbellApp(directoryName) {
+// main
 
-  const template = getTemplate();
-  const installCommand = await getInstaller();
-  
-  const execOptions = {
-    stdio: [0, 1, 2]
-  }
-  
-  if (template === 'default' || template === 'minimal') {
-    // copy default template from templates/default
-    const templatesDir = path.join(__dirname, '..', 'templates')
-    copyFolderSync(
-      path.join(templatesDir, template), 
-      directoryName, 
-      [path.join(templatesDir, template, 'node_modules')]
-    )
+/** 
+ * create-abell-app [projectName] --template <template> --installer <installer>
+ */
+program
+  .option('-t|--template <template>', 'Specify template for abell app')
+  .option('-i|--installer <installer>', 'Specify package installer. npm or yarn.')
+  .arguments('[projectName]')
+  .action(async (projectName) => {
+    // if projectName is falsy, prompt user for new name
+    if (!projectName) {
+      projectName = (await prompts({
+        type: 'text',
+        message: 'Enter Name of your project',
+        name: 'projectName',
+        initial: 'hello-world'
+      })).projectName;
+    }
+
+    projectName = projectName.toLowerCase().replace(/ |_/g, '-');
+    const template = getTemplate();
+    const installCommand = await getInstallCommand();
+    const execOptions = {
+      stdio: [0, 1, 2]
+    }
+    
+    console.log(`\n${colors.green('>>')} Creating New Abell Project 🌻`) 
+    console.log(`${colors.cyan('Directory:')} ${projectName}\n${colors.cyan('Template:')} ${template}\n${colors.cyan('Package Installer:')} ${installCommand}\n`);
+    
+    
+    // Start creating new project
+
+    if (template === 'default' || template === 'minimal') {
+      // copy default template from templates directory
+      const templatesDir = path.join(__dirname, '..', 'templates')
+      copyFolderSync(
+        path.join(templatesDir, template), 
+        projectName, 
+        [path.join(templatesDir, template, 'node_modules')]
+      )
+    } else {
+      // Execute git clone
+      console.log(`\n${colors.green('>')} Fetching Template from GitHub ✨\n\n`);
+      execSync(`git clone ${template} ${projectName}`, execOptions)
+    }
+
+    // remove package-lock.json if preferred installer is yarn
+    if (installCommand === 'yarn') {
+      fs.unlinkSync(path.join(projectName, 'package-lock.json'));
+    }
+
+    // Delete git history
+    rmdirRecursiveSync(path.join(projectName, '.git'));
+
+    // Install dependencies
+    console.log(`\n${colors.green('>')} Installing Dependencies 📚\n\n`);
+    execSync(`cd ${projectName} && ${installCommand}`, execOptions)
+
+    // Finish log
+    console.log(`\n${colors.green('>')} Finished Installing Dependencies 🤠`);
+    console.log(`${colors.green('>')} Successfully Created ${projectName} 🚀`);
+    console.log(`\n${colors.green('>>')} \`cd ${projectName}\` and \`${installCommand === 'yarn' ? 'yarn' : 'npm run'} dev\` to run dev-server ✨\n\n`);
+  });
+
+
+// Functions
+async function getInstallCommand() {
+  if (!program.installer) {
+    // if installer flag is undefined, ask user.
+    const answers = await prompts({
+      type: 'select',
+      message: 'Select Installer',
+      name: 'installCommand',
+      choices: [
+        {
+          title: 'npm',
+          value: 'npm install'
+        },
+        {
+          title: 'yarn',
+          value: 'yarn'
+        }
+      ]
+    })
+
+    return answers.installCommand;
+  } 
+
+  if (program.installer === 'yarn') {
+    return 'yarn'
   } else {
-    // Execute git clone
-    console.log(`\n${colors.green('>>')} Fetching Template from GitHub ✨\n\n`);
-    execSync(`git clone ${template} ${directoryName}`, execOptions)
+    return 'npm install'
   }
-  
-
-  if (installCommand === 'yarn') {
-    fs.unlinkSync(path.join(directoryName, 'package-lock.json'))
-  }
-  // Delete git history
-  rmdirRecursiveSync(path.join(directoryName, '.git'));
-  
-  // Install dependencies
-  console.log(`\n${colors.green('>>')} Installing Dependencies 📚\n\n`);
-  execSync(`cd ${directoryName} && ${installCommand}`, execOptions)
-  
-  // Finish log
-  console.log(`\n${colors.green('>>')} Finished Installing 🚀`);
-  console.log(`${colors.green('>>')} \`cd ${directoryName}\` and \`${installCommand === 'yarn' ? 'yarn' : 'npm run'} dev\` to run dev server ✨\n\n`);
 }
 
-// Main 
-(async () => {
-  const args = process.argv.slice(2);
-  
-  let command = args[0];
-  if (!command) {
-    command = (await prompts({
-      type: 'text',
-      message: 'Enter Name of your project',
-      name: 'projectName',
-      initial: 'hello-world'
-    })).projectName;
-  }
-  
-  if (command.toLowerCase() === '-v' || command.toLowerCase() === '--version') {
-    console.log('v' + require('../package.json').version);
-  } else {
-    createAbellApp(command);
-  }
-})();
 
+function getTemplate() {
+  // return default when value is not defined
+  if (!program.template) return 'default'; 
 
-module.exports = createAbellApp
+  if (program.template === 'default' || program.template === 'minimal') {
+    // 'default' and 'minimal' are valid templates. Return them as it is
+    return program.template;
+  }
+
+  // when `--template abelljs/abell-starter-portfolio`
+  if (!program.template.startsWith('https://github.com/')) {
+    // If template value is `abelljs/abell-starter-portfolio`, add https://github.com before it.
+    return 'https://github.com/' + program.template;
+  }
+
+  // when `--template https://github.com/abelljs/abell-starter-portfolio`
+  return program.template;
+}
+  
+program.parse(process.argv);
--- templates/default/content/deployment/index.md
+++ templates/default/content/deployment/index.md
@@ -1,3 +1,35 @@
 # Deployment
 
-Hi.
+You can either Deploy from a GitHub Repository or Deploy folder manually.
+
+## Deploy with GitHub
+
+### 1. Create GitHub Repository
+
+- Create a GitHub repository on your GitHub account
+- Push the content from this project to your repository.
+```
+git init
+git remote add origin <your-github-project-url>
+git add .
+git commit -m "init"
+git push origin master # Replace 'master' with your default branch name
+```
+
+### 2. Create Project on Netlify (or other hosting platform)
+
+- Go to [Netlify](https://netlify.com/) (or any other hosting platform you like)
+- Create new project/site from your new GitHub repository to the project.
+
+### 3. Set Deploy Options
+
+Set deploy options to -<br/><br/>
+**Build Command:** npm run build<br/>
+**Publish Directory:** dist
+
+## Deploy Folder
+
+- Open Netlify and login to your account.
+- Run `npm run build` from root of your project.
+- Drag-Drop new `dist` directory to Netlify window.
+
