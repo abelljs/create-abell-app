39e812825deee283b935b8ca9fdd524569c03666
saurabhdaware <saurabhdaware99@gmail.com>
2020-12-27 20:30:46 +0530

    comments(default template): Add comments to code

--- templates/default/abell.config.js
+++ templates/default/abell.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  themePath: 'theme',
-  outputPath: 'dist',
+  themePath: 'theme', // Source files of the blog in 'theme' directory.
+  contentPath: 'content', // Content of the blog is in 'content' directory.
+  outputPath: 'dist', // On npm run build, website is created in 'dist' directory.
   globalMeta: {
     title: 'Abell Default Template',
     shortName: 'Abell'
